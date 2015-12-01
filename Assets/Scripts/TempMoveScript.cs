62131a1f7e3fcda63914fd15f40ad883bfa410d0
Voidraizer <splutikoff@gmail.com>
2015-12-01 18:35:16 -0500

    Added temporary movement script for testing

---
+++ Assets/Scripts/TempMoveScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TempMoveScript : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if( Input.GetKey( KeyCode.W ) )
+        {
+            transform.Translate( 0f, moveSpeed, 0f );
+        }
+        if( Input.GetKey( KeyCode.A ) )
+        {
+            transform.Translate( -moveSpeed, 0f, 0f );
+        }
+        if( Input.GetKey( KeyCode.S ) )
+        {
+            transform.Translate( 0f, -moveSpeed, 0f );
+        }
+        if( Input.GetKey( KeyCode.D ) )
+        {
+            transform.Translate( moveSpeed, 0f, 0f );
+        }
+    }
+}
