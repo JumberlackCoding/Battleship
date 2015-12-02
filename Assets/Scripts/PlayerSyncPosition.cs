677a49191c0269471a9a4c2448022e786bd76612
Voidraizer <splutikoff@gmail.com>
2015-12-02 00:13:47 -0500

    Finished Part 1
    Added some lerping so the players see other players moving smoothly
    
    Added a command and client callback in order to basically make the
    clients and servers talk and say, I want you to take my position and
    give it to the other clients and then lerp it so it move smoothly.
    
    Party 2 is here:
    https://www.youtube.com/watch?v=PVUT6SR70wg

---
+++ Assets/Scripts/PlayerSyncPosition.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerSyncPosition : NetworkBehaviour {
+
+    [SerializeField]
+    private Transform myTransform;
+
+    [SerializeField]
+    private float lerpRate = 15;
+
+    [SyncVar] // Tells server to take this value and synchronize it among the clients
+    private Vector3 syncPos;
+
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        TransmitPosition();
+        LerpPosition();
+	}
+
+    void LerpPosition()
+    {
+        if( !isLocalPlayer )
+        {
+            myTransform.position = Vector3.Lerp( myTransform.position, syncPos, Time.deltaTime * lerpRate );
+        }
+    }
+
+    [Command] // Sent to server.. Client is telling server to run this 
+    void CmdProvidePositionToSever( Vector3 pos ) // must be called Cmd*
+    {
+        syncPos = pos;
+    }
+
+    [ClientCallback] // Won't generate warnings
+    void TransmitPosition()
+    {
+        if( isLocalPlayer )
+        {
+            CmdProvidePositionToSever( myTransform.position );
+        }
+    }
+}
